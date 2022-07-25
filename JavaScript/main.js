de26a4634ca0eea4beda65a151be2bf4f8746822
Olga-Gvozdeva <atlasink@mail.ru>
2022-07-25 23:22:51 +0300

    done

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -499,14 +499,49 @@ function displaySize(){
             }
             
         }
+
+        /*
         let myMap;
         ymaps.ready(init);
         function init () {
             myMap = new ymaps.Map('mapShops', {
                 center: [55.782860, 37.669178],
-                zoom: 18
+                zoom: 17
             }, {
                 searchControlProvider: 'yandex#search'
             });
             
         }
+        */
+
+        ymaps.ready(function () {
+            let myMap = new ymaps.Map('mapShops', {
+                    center: [55.782860, 37.669178],
+                    zoom: 12
+                }, {
+                    searchControlProvider: 'yandex#search'
+                }),
+        
+                MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                    '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+                ),
+        
+                myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                    hintContent: 'Собственный значок метки',
+                    balloonContent: 'Это красивая метка'
+                }, {
+                    // Опции.
+                    // Необходимо указать данный тип макета.
+                    iconLayout: 'default#image',
+                    // Своё изображение иконки метки.
+                    iconImageHref: '../Img/person.png',
+                    // Размеры метки.
+                    iconImageSize: [30, 42],
+                    // Смещение левого верхнего угла иконки относительно
+                    // её "ножки" (точки привязки).
+                    iconImageOffset: [-5, -38]
+                });
+        
+            myMap.geoObjects
+                .add(myPlacemark)
+        });
